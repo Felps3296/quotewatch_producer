db7a94eb6e09a31f536bda6f0fe605ffa1cf630e
Felps3296 <felipereis.live@gmail.com>
2026-07-13 16:14:00 -0300

    Criando teste automatizado mapper

---
+++ src/test/java/com/felipereis/quotewatch/producer/mapper/CotacaoMapperTest.java
@@ -0,0 +1,43 @@
+package com.felipereis.quotewatch.producer.mapper;
+
+import com.felipereis.quotewatch.producer.dto.CotacaoExternaDTO;
+import com.felipereis.quotewatch.producer.model.Cotacao;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CotacaoMapperTest {
+
+    private final CotacaoMapper mapper = new CotacaoMapper();
+
+    @Test
+    void deveConverterCotacaoExternaParaEntidadeCorretamente() {
+        // Arrange: monta um cenario fake
+        CotacaoExternaDTO externa = new CotacaoExternaDTO(
+                "USD",
+                "BRL",
+                "Dolar Americano/Real Brasileiro",
+                "5.16149",
+                "5.0994",
+                "-0.0275",
+                "-0.535658",
+                "5.1064",
+                "5.1164",
+                "1783718838",
+                "2026-07-10 18:27:18"
+        );
+
+        // Act: executa o metodo que estamos testando
+        Cotacao resultado = mapper.toEntity(externa);
+
+        // Assert: confere se o resultado e exatamente o que esperamos
+        assertThat(resultado.getMoeda()).isEqualTo("USD");
+        assertThat(resultado.getValorCompra()).isEqualByComparingTo(new BigDecimal("5.1064"));
+        assertThat(resultado.getValorVenda()).isEqualByComparingTo(new BigDecimal("5.1164"));
+        assertThat(resultado.getVariacaoPct()).isEqualByComparingTo(new BigDecimal("-0.535658"));
+        assertThat(resultado.getDataCotacao()).isEqualTo(LocalDateTime.of(2026, 7, 10, 18, 27, 18));
+    }
+}
